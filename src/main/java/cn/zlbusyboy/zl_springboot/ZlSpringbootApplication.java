544c9aaf5ac98d98a56b89996295e001621a7ce9
ningfei.li <ningfei.li@gloryholiday.com>
2018-09-09 22:04:47 +0800

    springboot init

---
+++ src/main/java/cn/zlbusyboy/zl_springboot/ZlSpringbootApplication.java
@@ -0,0 +1,12 @@
+package cn.zlbusyboy.zl_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZlSpringbootApplication.class, args);
+	}
+}
