7607c58f29f1d7ba01097d31a2e02705e8423052
ningfei.li <ningfei.li@gloryholiday.com>
2019-05-21 16:43:48 +0800

    spring boot no use import

--- spingboot_demo/src/main/java/cn/busyboy/spingboot_demo/SpingbootDemoApplication.java
+++ spingboot_demo/src/main/java/cn/busyboy/spingboot_demo/SpingbootDemoApplication.java
@@ -1,11 +1,8 @@
 package cn.busyboy.spingboot_demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableConfigurationProperties
